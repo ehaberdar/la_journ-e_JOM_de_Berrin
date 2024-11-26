7a8cb5dee64acfb1f2d9427dd909fb2b8b1bdd3c
Ertugrul Haberdar <118924687+ehaberdar@users.noreply.github.com>
2024-11-26 13:04:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Sayfa tamamen yüklendikten sonra çalışacak fonksiyon
 window.onload = function () {
   alert(
-    "Bienvenue sur mon site internet dédié à la présentation de la profession d'éducatrice d'enfants, préparé pour JOM :)"
+    "Bienvenue sur mon site internet dédié à la présentation de la profession d'éducateur / éducatrice de l'enfance, préparé pour JOM :)"
   );
 };
